173aa003f975465cd721986ea237fcb490d4078c
tallguy9271 <drewoken@comcast.net>
2015-11-14 17:18:11 -0800

    more changes
    lol idk what i changed, why are you asking me?

--- PictureThis/app/src/main/java/hacksc2015/myapplication/MainMenuActivity.java
+++ PictureThis/app/src/main/java/hacksc2015/myapplication/MainMenuActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,11 +36,33 @@ public class MainMenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //code for when button is pressed
-                Intent intent = new Intent();
+                Intent intent = new Intent("MAKE_A_GUESS");
                 startActivity(intent);
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
